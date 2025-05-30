87f3a6bbe7993eb021d3556586fcf6a6de8872e9
AtharvaPL <atharvalonhari@gmail.com>
2025-05-30 13:02:32 +0530

    gluestack

--- ExpenseTracker/app/_layout.tsx
+++ ExpenseTracker/app/_layout.tsx
@@ -1,4 +1,6 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
@@ -67,13 +69,15 @@ export default function RootLayout() {
   }
 
   return (
-    <AuthProvider>
-      <Provider store={store}>
-        <PaperProvider theme={paperLightTheme}>
-          <RootLayoutNav />
-        </PaperProvider>
-      </Provider>
-    </AuthProvider>
+      <AuthProvider>
+        <Provider store={store}>
+          <GluestackUIProvider mode="light">
+            <PaperProvider theme={paperLightTheme}>
+              <RootLayoutNav />
+            </PaperProvider>
+          </GluestackUIProvider>
+        </Provider>
+      </AuthProvider>
   );
 }
 
@@ -92,47 +96,47 @@ function RootLayoutNav() {
   };
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="auth/welcome" options={{ headerShown: false }} />
-        <Stack.Screen name="auth/login" options={{ headerShown: false }} />
-        <Stack.Screen name="auth/signup" options={{ headerShown: false }} />
-        <Stack.Screen name="auth/forget" options={{ headerShown: false }} />
-        <Stack.Screen name="auth/otp" options={{ headerShown: false }} />
-        <Stack.Screen name="auth/reset" options={{ headerShown: false }} />
-        <Stack.Screen name="action/create/createExpense" options={{headerShown:false}}/>
-        <Stack.Screen name="action/create/createTransaction" options={{headerShown:false}}/>
-        <Stack.Screen name="action/create/addFriends" options={{headerShown:false}}/>
-        <Stack.Screen name="action/create/addPeopleManual" options={{headerShown:false}}/>
-        <Stack.Screen name="action/create/createGroup" options={{headerShown:false}}/>
-        <Stack.Screen name="action/create/createBill" options={{headerShown:false}}/>
-        <Stack.Screen name="action/create/createWallet" options={{headerShown:false}}/>
-        <Stack.Screen name="action/create/addPeopleEmail" options={{headerShown:false}}/>
-        <Stack.Screen name="action/create/createSettlement" options={{headerShown:false}}/>
-        <Stack.Screen name="action/create/createBudget" options={{headerShown:false}}/>
-        <Stack.Screen name="view/viewPeople" options={{headerShown:false}}/>
-        <Stack.Screen name="view/viewBudget" options={{headerShown:false}}/>
-        <Stack.Screen name="view/viewPredictedBudget" options={{headerShown:false}}/>
-        <Stack.Screen name="view/viewProfile" options={{headerShown:false}}/>
-        <Stack.Screen name="view/viewExpense" options={{headerShown:false}}/>
-        <Stack.Screen name="view/viewTransaction" options={{headerShown:false}}/>
-        <Stack.Screen name="view/viewGroup" options={{headerShown:false}}/>
-        <Stack.Screen name="view/viewWallet" options={{headerShown:false}}/>
-        <Stack.Screen name="view/viewBill" options={{headerShown:false}}/>
-        <Stack.Screen name="view/viewSettlement" options={{headerShown:false}}/>
-        <Stack.Screen name="action/edit/editSettlement" options={{headerShown:false}}/>
-        <Stack.Screen name="action/edit/editExpense" options={{headerShown:false}}/>
-        <Stack.Screen name="action/edit/editBill" options={{headerShown:false}}/>
-        <Stack.Screen name="action/edit/editTransaction" options={{headerShown:false}}/>
-        <Stack.Screen name="action/edit/editWallet" options={{headerShown:false}}/>
-        <Stack.Screen name="action/edit/editBudget" options={{headerShown:false}}/>
-        <Stack.Screen name="action/edit/editGroup" options={{headerShown:false}}/>
-        <Stack.Screen name="action/edit/editProfile" options={{headerShown:false}}/>
-        <Stack.Screen name="misc/groupSettings" options={{headerShown:false}}/>
-        <Stack.Screen name="misc/privacyPolicy" options={{headerShown:false}}/>
-        <Stack.Screen name="misc/contactUs" options={{headerShown:false}}/>
-      </Stack>
-    </ThemeProvider>
+    <GluestackUIProvider mode="light"><ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="auth/welcome" options={{ headerShown: false }} />
+          <Stack.Screen name="auth/login" options={{ headerShown: false }} />
+          <Stack.Screen name="auth/signup" options={{ headerShown: false }} />
+          <Stack.Screen name="auth/forget" options={{ headerShown: false }} />
+          <Stack.Screen name="auth/otp" options={{ headerShown: false }} />
+          <Stack.Screen name="auth/reset" options={{ headerShown: false }} />
+          <Stack.Screen name="action/create/createExpense" options={{headerShown:false}}/>
+          <Stack.Screen name="action/create/createTransaction" options={{headerShown:false}}/>
+          <Stack.Screen name="action/create/addFriends" options={{headerShown:false}}/>
+          <Stack.Screen name="action/create/addPeopleManual" options={{headerShown:false}}/>
+          <Stack.Screen name="action/create/createGroup" options={{headerShown:false}}/>
+          <Stack.Screen name="action/create/createBill" options={{headerShown:false}}/>
+          <Stack.Screen name="action/create/createWallet" options={{headerShown:false}}/>
+          <Stack.Screen name="action/create/addPeopleEmail" options={{headerShown:false}}/>
+          <Stack.Screen name="action/create/createSettlement" options={{headerShown:false}}/>
+          <Stack.Screen name="action/create/createBudget" options={{headerShown:false}}/>
+          <Stack.Screen name="view/viewPeople" options={{headerShown:false}}/>
+          <Stack.Screen name="view/viewBudget" options={{headerShown:false}}/>
+          <Stack.Screen name="view/viewPredictedBudget" options={{headerShown:false}}/>
+          <Stack.Screen name="view/viewProfile" options={{headerShown:false}}/>
+          <Stack.Screen name="view/viewExpense" options={{headerShown:false}}/>
+          <Stack.Screen name="view/viewTransaction" options={{headerShown:false}}/>
+          <Stack.Screen name="view/viewGroup" options={{headerShown:false}}/>
+          <Stack.Screen name="view/viewWallet" options={{headerShown:false}}/>
+          <Stack.Screen name="view/viewBill" options={{headerShown:false}}/>
+          <Stack.Screen name="view/viewSettlement" options={{headerShown:false}}/>
+          <Stack.Screen name="action/edit/editSettlement" options={{headerShown:false}}/>
+          <Stack.Screen name="action/edit/editExpense" options={{headerShown:false}}/>
+          <Stack.Screen name="action/edit/editBill" options={{headerShown:false}}/>
+          <Stack.Screen name="action/edit/editTransaction" options={{headerShown:false}}/>
+          <Stack.Screen name="action/edit/editWallet" options={{headerShown:false}}/>
+          <Stack.Screen name="action/edit/editBudget" options={{headerShown:false}}/>
+          <Stack.Screen name="action/edit/editGroup" options={{headerShown:false}}/>
+          <Stack.Screen name="action/edit/editProfile" options={{headerShown:false}}/>
+          <Stack.Screen name="misc/groupSettings" options={{headerShown:false}}/>
+          <Stack.Screen name="misc/privacyPolicy" options={{headerShown:false}}/>
+          <Stack.Screen name="misc/contactUs" options={{headerShown:false}}/>
+        </Stack>
+      </ThemeProvider></GluestackUIProvider>
   );
 }
